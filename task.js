416c9175b67a311db374e43cf26a7e843bf9b647
Lahiru Udawaththa <45584797+lahiruroot@users.noreply.github.com>
2019-10-23 16:19:45 +0530

    Update task.js

--- task.js
+++ task.js
@@ -103,4 +103,18 @@ keys.addEventListener('click', (event) => {
 
     inputDigit(target.value);
     updateDisplay();
+    
 });
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
